3b14699e9899e1f79ecee188125c0543a91eb801
Øystein Dale <oystedal@ifi.uio.no>
2018-05-21 16:28:53 +0200

    polybar config update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,9 +8,8 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar example &
 
 for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -c ~/.config/polybar/config & done
-feh --bg-scale ~/.config/wall.png
+# for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar example -l info -c ~/.config/polybar/config & done
 
 echo "Bars launched..."
